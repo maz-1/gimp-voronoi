14ec17c58f848d195693b9c3bc492a1520f39376
maz-1 <ohmygod19993@gmail.com>
2015-01-26 19:46:00 +0800

    update headers

--- libgimp/stdplugins-intl.h
+++ libgimp/stdplugins-intl.h
@@ -1,22 +1,21 @@
-/* LIBGIMP - The GIMP Library 
- * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball 
+/* LIBGIMP - The GIMP Library
+ * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball
  *
  * stdplugins-intl.h
  *
- * This library is free software; you can redistribute it and/or
+ * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- * 
+ * version 3 of the License, or (at your option) any later version.
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __STDPLUGINS_INTL_H__
@@ -26,16 +25,7 @@
 #error "config.h must be included prior to stdplugins-intl.h"
 #endif
 
-#include <libintl.h>
-
-
-#define _(String) gettext (String)
-
-#ifdef gettext_noop
-#    define N_(String) gettext_noop (String)
-#else
-#    define N_(String) (String)
-#endif
+#include <glib/gi18n.h>
 
 #ifndef HAVE_BIND_TEXTDOMAIN_CODESET
 #    define bind_textdomain_codeset(Domain, Codeset) (Domain)
